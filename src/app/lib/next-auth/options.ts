a7f1e914d1e17aa97320388e234d4fb00c516d73
nkkn-prog <nkkn.prog@gmail.com>
2024-12-14 18:58:13 +0900

    create auth

---
+++ src/app/lib/next-auth/options.ts
@@ -0,0 +1,29 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../prisma";
+
+
+export const nextAuthOptions:NextAuthOptions = {
+    debug: false,
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+          }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+        session: ({session, user}) => {
+            return {
+                ...session.user,
+                expires: session.expires,
+                user: {
+                    ...session.user,
+                    id: user.id,
+                },
+            };
+        },
+    },
+
+}
